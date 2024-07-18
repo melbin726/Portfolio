40168732bc25415a45c3ca40da561318f3834f35
melbin726 <melbinjoseph726@gmail.com>
2024-07-18 17:32:27 +0530

    Fix ESLint errors and warnings

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ function Footer() {
   return (
     <Box component="footer" py={3} textAlign="center">
       <Typography variant="body2">
-        © 2024 MELBIN JOSEPH. All rights reserved.
+        © 2024 melbin joseph All rights reserved.
       </Typography>
       <Typography variant="body2">
         Follow Me
@@ -18,16 +18,16 @@ function Footer() {
           <Divider orientation="vertical" flexItem />
         </Box>
         <Link href="https://github.com/melbin726/" target="_blank">
-          <GitHubIcon sx={{ mr: 1 }} />
+          <GitHubIcon sx={{ mr: 1 ,color: 'white'}} />
         </Link>
         <Link href="https://www.linkedin.com/in/melbin-joseph-96640a252/" target="_blank">
-          <LinkedInIcon sx={{ mx: 1 }} />
+          <LinkedInIcon sx={{ mx: 1 ,color: 'white'}} />
         </Link>
         <Link href=" https://x.com/Melbin?s=08" target="_blank">
-          <TwitterIcon sx={{ ml: 1 }} />
+          <TwitterIcon sx={{ ml: 1,color: 'white' }} />
         </Link>
-        <Link href="https://www.instagram.com/___melbin_/?igsh=MXFmcHN4NzYwajM0eA%3D%3D" target="_blank">
-          <InstagramIcon sx={{ ml: 1 }} />
+        <Link href="https://www.instagram.com/___melbin_/?igsh=MXFmcHN4NzYwajM0eA%3D%3D" target="_blank" sxcolor={'white'}>
+          <InstagramIcon sx={{ ml: 1 , color: 'white'}} />
         </Link>
       </Typography>
     </Box>
