02c0f9f61b4b90b2fc8e26c005e77a02514792af
melbin726 <141119619+melbin726@users.noreply.github.com>
2025-02-07 10:53:08 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,19 @@
-// src/pages/Home.js
 import React, { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Container, Typography, Box, Grid, Button } from '@mui/material';
 import { Tilt } from 'react-tilt';
 import Typed from 'typed.js';
 import { styled } from '@mui/system';
+import { motion } from 'framer-motion';
 
-const AnimatedTypography = styled(Typography)({
+const AnimatedTypography = styled(motion(Typography))({
   transition: 'transform 0.3s ease-in-out',
   '&:hover': {
     transform: 'scale(1.1)',
   },
 });
 
-const AnimatedImage = styled('div')({
+const AnimatedImage = styled(motion.div)({
   transition: 'transform 0.3s ease-in-out',
   '&:hover': {
     transform: 'scale(1.1)',
@@ -61,36 +61,39 @@ function WhatIDo() {
   return (
     <SectionContainer>
       <Container>
-      <AnimatedTypography
+        <AnimatedTypography
           variant="h3"
           component="h1"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 1 }}
         >
-        <SectionTitle variant="h4" component="h2">What I Do </SectionTitle></AnimatedTypography>
+          <SectionTitle variant="h4" component="h2">What I Do</SectionTitle>
+        </AnimatedTypography>
         <Grid container spacing={4}>
           <Grid item xs={12} md={6}>
             <Typography variant="h5" component="h3" fontWeight="bold">Web Development</Typography>
             <SectionContent variant="body1">
-            Utilizing React.js, HTML, CSS,JavaScript, C# ,sql, postgresql.....etc I specialize in developing dynamic and responsive web applications. My passion lies in writing clean, modular, and maintainable code to build robust web solutions.
+              Utilizing React.js, HTML, CSS, JavaScript, C#, SQL, PostgreSQL, etc., I specialize in developing dynamic and responsive web applications. My passion lies in writing clean, modular, and maintainable code to build robust web solutions.
             </SectionContent>
           </Grid>
           <Grid item xs={12} md={6}>
             <Typography variant="h5" component="h3" fontWeight="bold">Responsive UI Design</Typography>
             <SectionContent variant="body1">
-            "A user interface is like a joke. If you have to explain it, it's not that good." I am committed to designing and implementing interfaces that are not only visually appealing but also highly intuitive. My goal is to ensure a seamless user experience across all devices and platforms, employing best practices in responsive design and accessibility.            </SectionContent>
+              "A user interface is like a joke. If you have to explain it, it's not that good." I am committed to designing and implementing interfaces that are not only visually appealing but also highly intuitive. My goal is to ensure a seamless user experience across all devices and platforms, employing best practices in responsive design and accessibility.
+            </SectionContent>
           </Grid>
           <Grid item xs={12} md={6}>
             <Typography variant="h5" component="h3" fontWeight="bold">Experience Design</Typography>
             <SectionContent variant="body1">
-            User experience is at the heart of my development philosophy. I focus on creating intuitive and enjoyable interactions that minimize frustration and maximize satisfaction. My experience in user-centered design principles helps me develop interfaces that are easy to navigate and use, ensuring that the end product meets the needs and expectations of its users.
+              User experience is at the heart of my development philosophy. I focus on creating intuitive and enjoyable interactions that minimize frustration and maximize satisfaction. My experience in user-centered design principles helps me develop interfaces that are easy to navigate and use, ensuring that the end product meets the needs and expectations of its users.
             </SectionContent>
           </Grid>
           <Grid item xs={12} md={6}>
             <Typography variant="h5" component="h3" fontWeight="bold">Diversity in Tech</Typography>
             <SectionContent variant="body1">
-            Beyond my technical skills, I am a strong advocate for diversity and inclusion in the tech industry. I believe that a diverse and inclusive environment fosters innovation and growth. I actively support initiatives that promote diversity, aiming to create a more equitable and welcoming tech community for everyone.            </SectionContent>
+              Beyond my technical skills, I am a strong advocate for diversity and inclusion in the tech industry. I believe that a diverse and inclusive environment fosters innovation and growth. I actively support initiatives that promote diversity, aiming to create a more equitable and welcoming tech community for everyone.
+            </SectionContent>
           </Grid>
         </Grid>
       </Container>
@@ -137,7 +140,7 @@ function Home() {
               </Typography>
             </Box>
             <Typography variant="body1" component="p" fontFamily={'Marker Felt'} mb={2}>
-            I am a dedicated software engineer currently working at the Kristu Jayanti Software Development Center, where I focus on building dynamic web applications using Angular, Tailwind CSS, and Java. With hands-on experience in both frontend and backend technologies, I strive to create seamless user experiences and efficient, scalable solutions. My previous internships at MicroGenesis TechSoft and Talview have honed my skills in web development, where I consistently demonstrated professionalism and a commitment to delivering quality work.
+              I am a dedicated software engineer currently working at the Kristu Jayanti Software Development Center, where I focus on building dynamic web applications using Angular, Tailwind CSS, and Java. With hands-on experience in both frontend and backend technologies, I strive to create seamless user experiences and efficient, scalable solutions. My previous internships at MicroGenesis TechSoft and Talview have honed my skills in web development, where I consistently demonstrated professionalism and a commitment to delivering quality work.
             </Typography>
             <Box mt={2}>
               <PortfolioButton
@@ -155,7 +158,11 @@ function Home() {
           </Grid>
           <Grid item xs={12} md={6} display="flex" justifyContent="center">
             <Tilt options={{ max: 25, scale: 1.05 }}>
-              <AnimatedImage>
+              <AnimatedImage
+                initial={{ opacity: 0, y: 50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1 }}
+              >
                 <img
                   src="/Minimalist_Platform_Delivery_Courier_Website_Desktop_Prototype_Desktop_Prototype__2_-removebg-preview.png"
                   alt="Anonymous"
